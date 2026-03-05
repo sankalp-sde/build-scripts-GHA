aef9f40fd284907c9e4058d052287823676e861f
Sankalp <Sankalp@ibm.com>
2026-03-05 16:23:18 +0530

    Update trivy_code_scan.sh

--- script/scanner-scripts/trivy_code_scan.sh
+++ script/scanner-scripts/trivy_code_scan.sh
@@ -7,20 +7,40 @@ cd package-cache
 DOCKER_IMAGE="sankalppersi/trivy-db:latest"
 docker pull "$DOCKER_IMAGE"
 
-if [ $validate_build_script == true ];then
-    wget https://github.com/aquasecurity/trivy/releases/download/v0.45.0/trivy_0.45.0_Linux-S390X.tar.gz
-    tar -xf trivy_0.45.0_Linux-S390X.tar.gz
-    chmod +x trivy
-    sudo mv trivy /usr/bin
-    sudo trivy -q fs --timeout 30m -f json "${cloned_package}" > trivy_source_vulnerabilities_results.json || true
-    echo "trivy.db not found, copying again.."
-    find / -name "trivy.db" 2>/dev/null
-    sudo chmod -R 755 "$HOME/.cache/trivy/db"
-    sudo docker run -d --name trivy-container "$DOCKER_IMAGE"
-    sudo docker cp trivy-container:/trivy.db $HOME/.cache/trivy/db/trivy.db
-    sudo docker rm -f trivy-container
-    
-	sudo trivy -q fs --timeout 30m -f json ${cloned_package} > trivy_source_vulnerabilities_results.json
-	sudo trivy -q fs --timeout 30m -f cyclonedx ${cloned_package} > trivy_source_sbom_results.cyclonedx
- 	cat trivy_source_sbom_results.cyclonedx    
+if [ "$validate_build_script" == true ]; then
+
+    echo "[INFO] Fetching latest Trivy version..."
+    TRIVY_VERSION=$(curl -s https://api.github.com/repos/aquasecurity/trivy/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+    FILE_NAME="trivy_${TRIVY_VERSION#v}_Linux-PPC64LE.tar.gz"
+    CHECKSUM_FILE="trivy_${TRIVY_VERSION#v}_checksums.txt"
+
+    echo "[INFO] Downloading Trivy binary..."
+    wget -q https://github.com/aquasecurity/trivy/releases/download/${TRIVY_VERSION}/${FILE_NAME}
+
+    echo "[INFO] Downloading checksum file..."
+    wget -q https://github.com/aquasecurity/trivy/releases/download/${TRIVY_VERSION}/${CHECKSUM_FILE}
+    echo "[INFO] Verifying checksum..."
+
+    if grep "${FILE_NAME}" "${CHECKSUM_FILE}" | sha256sum --check --status; then
+        echo "[INFO] Checksum verification successful."
+        echo "[INFO] Installing Trivy..."
+        tar -xzf ${FILE_NAME}
+        chmod +x trivy
+        sudo mv trivy /usr/bin
+        echo "[INFO] Executing Trivy scanner..."
+		sudo trivy -q fs --timeout 30m -f json "${cloned_package}" > trivy_source_vulnerabilities_results.json || true
+	    echo "trivy.db not found, copying again.."
+	    find / -name "trivy.db" 2>/dev/null
+		find / -type d -path "*/.cache/trivy/db" 2>/dev/null
+	#   sudo chmod -R 755 "/usr/local/share/.cache/trivy/db"
+	    sudo docker run -d --name trivy-container "$DOCKER_IMAGE"
+	    sudo docker cp trivy-container:/trivy.db /root/.cache/trivy/db/trivy.db
+	    sudo docker rm -f trivy-container
+        sudo trivy -q fs --timeout 30m -f json ${cloned_package} > trivy_source_vulnerabilities_results.json
+        sudo trivy -q fs --timeout 30m -f cyclonedx ${cloned_package} > trivy_source_sbom_results.cyclonedx
+    else
+        echo "[ERROR] Checksum verification FAILED for ${FILE_NAME}."
+        echo "[ERROR] Trivy will NOT be installed due to integrity verification failure."
+        exit 1
+    fi
 fi
